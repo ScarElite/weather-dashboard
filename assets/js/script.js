c2c16ea59172fe2c8b740013bb7a64efb0b24ba2
Mitchell Armstrong <mra0211@gmail.com>
2022-09-02 20:28:55 -0400

    Added a displayCityButton function that takes the city the user inputs and creates a button below the ssearch button to act as a search history. Also added syling to the dynamically created buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var formInput = document.querySelector("#user-form");
 var cityInput = document.querySelector("#city-input");
+var cityHistoryEl = document.querySelector(".city-search-history")
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
@@ -7,6 +8,21 @@ var formSubmitHandler = function (event) {
     // get value from input
     var city = cityInput.value.trim();
     console.log(city);
-}
+    cityInput.value = "";
+    
+    displayCityButton(city);
+};
+
+var displayCityButton = function (cityName) {
+    console.log("Hello")
+    console.log(cityName);
+
+    var cityHistory = document.createElement("button");
+    cityHistory.className = "city-search";
+    // cityHistoryEl.setAttribute("href", cityName);
+    cityHistory.textContent = cityName;
+    cityHistoryEl.appendChild(cityHistory);
+    localStorage.setItem("city", cityName);
+};
 
 formInput.addEventListener("submit", formSubmitHandler);
